92ae95825940a8677d1d4160c7f5ee8577fcd136
Eoin Scanlon <scanlon.eoin@gmail.com>
2020-02-06 15:40:27 +0000

    Added K means and KNN algorithms

--- 1. Import and Explore.R
+++ 1. Import and Explore.R
@@ -13,6 +13,8 @@ library(grid)
 library(png)
 l#ibrary(downloader)
 library(grDevices)
+library(cluster) 
+library(factoextra)
 
 # Set wd and import
 train <- read.csv(file = 'train.csv')
@@ -90,4 +92,70 @@ dat.pca <- prcomp(scaled.dat)
 summary(dat.pca)
 # Not very useful
 
+# K means clustering
+clusters <- kmeans(scaled.dat, 5)
+str(clusters)
 
+# We want to figure out if there is an optimum number of clusters
+silhouette_score <- function(k){
+  km <- kmeans(scaled.dat, centers = k, nstart=25)
+  ss <- silhouette(km$cluster, dist(scaled.dat))
+  mean(ss[,3])
+}
+k <- 2:20
+avg_sil <- sapply(k, silhouette_score)
+plot(k, type='b', avg_sil, xlab='Number of clusters', ylab='Average Silhouette Scores', frame=FALSE)
+# It seems as though 16 might be the optimum number of clusters
+
+# Another visualisation
+fviz_nbclust(scaled.dat, kmeans, method='silhouette',k.max = 20)
+
+# Actual clustering 
+km.final <- kmeans(scaled.dat, 16)
+## Total Within cluster sum of square
+km.final$tot.withinss
+## Cluster sizes
+km.final$size
+sum(km.final$size) # 250 
+dat.km <- as.data.frame(scaled.dat)
+dat.km$cluster <- km.final$cluster
+
+
+# KNN 
+
+set.seed(123)
+# Add back in the target variable first
+scaled.dat <- cbind(scaled.dat,train$target)
+
+# break up the data into train and test 
+##Generate a random number that is 90% of the total number of rows in dataset.
+ran <- sample(1:nrow(scaled.dat), 0.9 * nrow(scaled.dat)) 
+
+##extract training set
+scaled.train <- scaled.dat[ran,] 
+##extract testing set
+scaled.test <- scaled.dat[-ran,] 
+
+library(class)
+pr <- knn(scaled.train[,0:300],scaled.test[,0:300],cl=scaled.train[,301],k=13)
+
+##create confusion matrix
+tab <- table(pr,scaled.test[,301])
+
+##this function divides the correct predictions by total number of predictions that tell us how accurate the model is.
+accuracy <- function(x){sum(diag(x)/(sum(rowSums(x)))) * 100}
+accuracy(tab)
+## 76%
+
+vector = c() # This is the empty vector where we will store our scores
+neighbour_score <- function(k){
+  pr <- knn(scaled.train[,0:300],scaled.test[,0:300],cl=scaled.train[,301],k)
+  tab <- table(pr,scaled.test[,301])
+  vector <- c(accuracy(tab))
+}
+k <- 2:20
+avg_neighbour <- sapply(k, neighbour_score)
+plot(k, type='b', avg_neighbour, xlab='Number of neighbours', ylab='Average Accuracy Scores (%)', frame=FALSE)
+# It seems as though 7 might be the optimum number of neighbours
+
+# Odd plot.
